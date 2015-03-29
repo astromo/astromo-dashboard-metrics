cfd37ef25e4ba9208a9804238e90d560c13d8728
Gilles De Mey <gilles.de.mey@gmail.com>
2015-03-29 18:10:41 +0200

    Added latency graph

---
+++ directives/chart.latency.js
@@ -0,0 +1,79 @@
+'use strict';
+
+angular.module('astromo.metrics')
+  .directive('astChartLatency', function(Restangular, $animate) {
+
+    return {
+      restrict    : 'EA',
+      scope       : {},
+      controller  : function($scope) {
+
+        this.update = function() {
+          Restangular.all('metrics').all('latency').getList()
+            .then(function(metrics) {
+              $scope.metrics = metrics;
+            });
+        };
+
+        this.update();
+      },
+      templateUrl : 'modules/dashboard-metrics/directives/chart.latency.html',
+      link        : function(scope, elem, attrs, controller) {
+        $animate.enabled(false, elem); // disable ngAnimate for this element
+
+        scope.loading = true;
+
+        scope.$watch('metrics', function(metrics) {
+          if (!metrics)
+            return;
+
+          metrics = _.map(metrics, function(metric, index) {
+            return {
+              x: new Date(metric[0]).getTime() / 1000,
+              y: metric[1],
+            };
+          });
+
+          render(metrics);
+        });
+
+        function render(metrics) {
+
+          var graph = new Rickshaw.Graph({
+            element  : elem.find('#chart__latency')[0],
+            width    : 500,
+            height   : 200,
+            renderer : 'area',
+            interpolation: 'linear',
+            series   : [{
+              color : '#18BC9C',
+              data  : metrics,
+              name  : 'Latency'
+            }]
+          });
+
+          new Rickshaw.Graph.Axis.X({
+            graph         : graph,
+            orientation   : 'bottom',
+            element       : elem.find('.x_axis')[0],
+            tickFormat    : function(t) {
+              return new Date(t * 1000).toLocaleTimeString('nl-BE');
+            }
+          });
+
+          new Rickshaw.Graph.Axis.Y( {
+            graph       : graph,
+            orientation : 'left',
+            element     : elem.find('.y_axis')[0],
+            tickFormat    : function(t) {
+              return t + 'ms';
+            }
+          });
+          graph.render();
+
+          scope.loading = false;
+        }
+      }
+    };
+
+  });
